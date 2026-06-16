4458ab9143f30568a1e117f74037903b3d66c314
Mohan <v.mohanraj2005@gmail.com>
2026-06-16 09:47:46 +0530

    initial commit

--- server/server.ts
+++ server/server.ts
@@ -23,7 +23,7 @@ app.use(cors(corsOptions))
 app.post('/api/stripe', express.raw({type: 'application/json'}), stripeWebhook)
 app.use(express.json());
 
-app.all('/api/auth/*', toNodeHandler(auth));
+app.use('/api/auth', toNodeHandler(auth));
 app.use(express.json({limit: '50mb'}))
 
 const port = process.env.PORT || 3000;
